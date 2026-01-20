85d1e59ebad49696a4c68cf6bf142e824546aef0
Lavanya <lavanyakodiganti21@gmail.com>
2026-01-20 12:07:13 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,17 @@
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n = 10; 
+        int firstTerm = 0;
+        int secondTerm = 1;
+
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(firstTerm + ", ");
+
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
+}
